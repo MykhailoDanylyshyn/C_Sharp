aff30206183b9c2bcf6aff1a0757cae0667d0cc5
Mykhailo Danylyshyn <mykh.danylyshyn@gmail.com>
2026-03-22 20:29:05 +0100

    did 1 task in practice_3

--- Practice_3/Program.cs
+++ Practice_3/Program.cs
@@ -1,1 +1,64 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq.Expressions;
+class MainClass
+{
+    static void Main()
+    {
+        ProcessTask1();
+        ProcessTask2();
+    }
+
+    static void ProcessTask1()
+    {
+        Console.WriteLine("Введіть арифметичний вираз: ");
+        string input = Console.ReadLine();
+
+        int result = 0;
+        string number = "";
+        char op = '+';
+
+        for (int i = 0; i < input.Length; i++)
+        {
+
+            if (char.IsDigit(input[i]))
+            {
+                number += input[i];
+            }
+            else if (input[i] == '+' || input[i] == '-')
+            {
+                if (number == "")
+                {
+                    op = input[i];
+                    continue;
+                }
+
+                if (op == '+')
+                    result += int.Parse(number);
+                else
+                    result -= int.Parse(number);
+
+                number = "";
+                op = input[i];
+            }
+        }
+
+        if (number != "")
+        {
+            if (op == '+')
+                result += int.Parse(number);
+            else
+                result -= int.Parse(number);
+        }
+
+        Console.WriteLine("Результат: " + result);
+
+    }
+
+    static void ProcessTask2()
+    {
+
+    }
+
+}
+
+
