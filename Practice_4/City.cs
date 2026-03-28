889bbe576e8ad1b0c481e9565e386a15d3b4724e
Mykhailo Danylyshyn <mykh.danylyshyn@gmail.com>
2026-03-28 17:35:16 +0100

    Practice_4 task 1

---
+++ Practice_4/City.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace CSharp.Classes
+{
+    class City
+    {
+        private string name;
+        private int population;
+        private string country;
+        private string districts;
+        private string code;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != "")
+                    name = value;
+            }
+        }
+
+        public int Population
+        {
+            get { return population; }
+            set
+            {
+                if (value > 0)
+                    population = value;
+            }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                if (value != "")
+                    country = value;
+            }
+        }
+
+        public string Districts
+        {
+            get { return districts; }
+            set
+            {
+                if (value != "")
+                    districts = value;
+            }
+        }
+
+        public string Code
+        {
+            get { return code; }
+            set
+            {
+                bool allnums = true;
+                foreach (char c in value)
+                {
+                    if (!Char.IsNumber(c))
+                        allnums = false;
+                }
+
+                if (value.Length >= 3 && value.Length < 5 && allnums)
+                    code = value;
+            }
+        }
+
+        public City(string Name, int Population, string Country, string Districts, string Code)
+        {
+            this.Name = Name;
+            this.Population = Population;
+            this.Country = Country;
+            this.Districts = Districts;
+            this.Code = Code;
+        }
+
+        public City() : this("NoName", 1, "Unknown", "None", "0000")
+        {
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{name}, {country}, {districts}, {code}. Population is {population}");
+        }
+
+        public void EditInfo()
+        {
+            Console.Write("Enter city name: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Enter population: ");
+            Population =Console.ReadLine();
+
+            Console.Write("Enter country: ");
+            Country = Console.ReadLine();
+
+            Console.Write("Enter districts: ");
+            Districts = Console.ReadLine();
+
+            Console.Write("Enter city code: ");
+            Code = Console.ReadLine();
+        }
+    }
+}
---
+++ Practice_4/Interface_City.cs
@@ -0,0 +1,12 @@
+﻿using CSharp.Classes;
+using System;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+City Kyiv = new();
+Kyiv.PrintInfo();
+
+Kyiv.EditInfo();
+Kyiv.PrintInfo();
