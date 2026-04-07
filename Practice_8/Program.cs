9e5edfe6a5462b0a9f0ad8db8fa701b915c2f278
Mykhailo Danylyshyn <mykh.danylyshyn@gmail.com>
2026-04-07 20:26:14 +0200

    fixed names issue

---
+++ Practice_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ClassLibraryPassport;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            ForeignPassport forpas = new ForeignPassport("UK", "John", "Doe", "AB", 12345, "Schengen");
+            forpas.Show();
+
+            Console.WriteLine();
+
+            Passport pas = forpas;
+            pas.Show();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+    }
+}
