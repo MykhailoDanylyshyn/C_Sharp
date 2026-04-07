3c094893409fcdd7cecb6c4b901eb6464f7c9e8d
Mykhailo Danylyshyn <mykh.danylyshyn@gmail.com>
2026-04-07 20:20:55 +0200

    Practice_8 and ClassLibraryPassport were added

---
+++ ClassLibraryPassport/Class1.cs
@@ -0,0 +1,52 @@
+﻿namespace ClassLibraryPassport
+{
+    public class Passport
+    {
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Series { get; set; }
+        public int Number { get; set; }
+
+        public Passport(string country, string name, string surname, string series, int number) 
+        { 
+            Country = country;
+            Name = name;
+            Surname = surname;
+            Series = series;
+            Number = number;
+        }
+
+        public Passport() : this(null, null, null, null, 0) { }
+
+        public void Show()
+        {
+            Console.WriteLine("Country: " + Country);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Surname: " + Surname);
+            Console.WriteLine($"Document series and number: {Series}{Number}");
+        }
+
+    }
+    public class ForeignPassport : Passport
+    {
+        new public string Series { get; set; }
+        new public int Number { get; set; }
+        public string Visas { get; set; }
+
+        public ForeignPassport (string country, string name, string surname, string series, int number, string visas)
+            : base(country,name, surname, series,number) 
+        { 
+            Series = series;
+            Number=number;
+            Visas = visas;
+        }
+
+        new public void Show()
+        { 
+            base.Show();
+            Console.WriteLine("Visas information: " + Visas);
+        }
+        
+    }
+}
