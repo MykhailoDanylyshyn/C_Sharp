9fc0535eccb762045d3f25634cd02bee5a31e7a1
Mykhailo Danylyshyn <mykh.danylyshyn@gmail.com>
2026-04-23 20:24:48 +0200

    practice

---
+++ Practice_23apr/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void ShowCurrentTime()
+    {
+        Console.WriteLine("Поточний час: " + DateTime.Now.ToLongTimeString());
+    }
+
+    static void ShowCurrentDate()
+    {
+        Console.WriteLine("Поточна дата: " + DateTime.Now.ToShortDateString());
+    }
+
+    static void ShowCurrentDayOfWeek()
+    {
+        Console.WriteLine("Сьогодні: " + DateTime.Now.DayOfWeek);
+    }
+
+    static void Client(Action method)
+    {
+        method();
+    }
+
+    static void Main()
+    {
+        Client(ShowCurrentTime);
+        Client(ShowCurrentDate);
+        Client(ShowCurrentDayOfWeek);
+    }
+}
